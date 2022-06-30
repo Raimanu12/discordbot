9f16ba0894a4301f928ea248d4da29b229726248
Raimanu12 <108442126+Raimanu12@users.noreply.github.com>
2022-06-29 15:56:13 -1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self,  message):
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswitch('hello!'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+
+client = MyClient()
+client.run('token')
+
